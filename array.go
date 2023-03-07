3efd248c028c930e400530b36daec95f19df27bd
Hung Tran <hung.p.tran@opswat.com>
2023-03-07 09:03:50 +0700

    feat: add array data structure

---
+++ array.go
@@ -0,0 +1,575 @@
+package gblink
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type ArrayError struct {
+	error
+}
+
+type Array[T constraints.Ordered] []T
+
+// Len returns the length of the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Len()) // 3
+func (a *Array[T]) Len() int {
+	return len(*a)
+}
+
+// Append appends the given value to the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array) // [1 2 3]
+func (a *Array[T]) Append(value T) {
+	*a = append(*a, value)
+}
+
+// AppendAll appends the given values to the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.AppendAll(1, 2, 3)
+//	fmt.Println(array) // [1 2 3]
+func (a *Array[T]) AppendAll(values ...T) {
+	*a = append(*a, values...)
+}
+
+// AppendArray appends the given array to the array.
+//
+// Example:
+//
+//	var array1 Array[int]
+//	array1.AppendArray(&Array[int]{1, 2, 3})
+//	fmt.Println(array1) // [1 2 3]
+func (a *Array[T]) AppendArray(array *Array[T]) {
+	*a = append(*a, *array...)
+}
+
+// AppendArrayAll appends the given arrays to the array.
+//
+// Example:
+//
+//	var array1 Array[int]
+//	array1.AppendArrayAll(&Array[int]{1, 2, 3}, &Array[int]{4, 5, 6})
+//	fmt.Println(array1) // [1 2 3 4 5 6]
+func (a *Array[T]) AppendArrayAll(arrays ...*Array[T]) {
+	for _, array := range arrays {
+		*a = append(*a, *array...)
+	}
+}
+
+// Clear clears the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+func (a *Array[T]) Clear() {
+	*a = (*a)[:0]
+}
+
+// Contains returns true if the array contains the given value.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Contains(2)) // true
+func (a *Array[T]) Contains(value T) bool {
+	for _, v := range *a {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsAll returns true if the array contains all the given values.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.ContainsAll(1, 2)) // true
+func (a *Array[T]) ContainsAll(values ...T) bool {
+	for _, value := range values {
+		if !a.Contains(value) {
+			return false
+		}
+	}
+	return true
+}
+
+// Some returns true if the array contains at least one value for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Some(func(value int) bool {
+//		return value > 2
+//	})) // true
+func (a *Array[T]) Some(predicate func(T) bool) bool {
+	for _, v := range *a {
+		if predicate(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// Every returns true if the array contains all values for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Every(func(value int) bool {
+//		return value > 0
+//	})) // true
+func (a *Array[T]) Every(predicate func(T) bool) bool {
+	for _, v := range *a {
+		if !predicate(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Filter returns a new array containing all values for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Filter(func(value int) bool {
+//		return value > 1
+//	})) // [2 3]
+func (a *Array[T]) Filter(predicate func(T) bool) *Array[T] {
+	var array Array[T]
+	for _, v := range *a {
+		if predicate(v) {
+			array.Append(v)
+		}
+	}
+	return &array
+}
+
+// Find returns the first value for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Find(func(value int) bool {
+//		return value > 1
+//	})) // 2
+func (a *Array[T]) Find(predicate func(T) bool) T {
+	for _, v := range *a {
+		if predicate(v) {
+			return v
+		}
+	}
+	var zero T
+	return zero
+}
+
+// FindIndex returns the index of the first value for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.FindIndex(func(value int) bool {
+//		return value > 1
+//	})) // 1
+func (a *Array[T]) FindIndex(predicate func(T) bool) int {
+	for i, v := range *a {
+		if predicate(v) {
+			return i
+		}
+	}
+	return -1
+}
+
+// FindLast returns the last value for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.FindLast(func(value int) bool {
+//		return value > 1
+//	})) // 3
+func (a *Array[T]) FindLast(predicate func(T) bool) T {
+	for i := len(*a) - 1; i >= 0; i-- {
+		if predicate((*a)[i]) {
+			return (*a)[i]
+		}
+	}
+	var zero T
+	return zero
+}
+
+// FindLastIndex returns the index of the last value for which the given predicate returns true.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.FindLastIndex(func(value int) bool {
+//		return value > 1
+//	})) // 2
+func (a *Array[T]) FindLastIndex(predicate func(T) bool) int {
+	for i := len(*a) - 1; i >= 0; i-- {
+		if predicate((*a)[i]) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Each calls the given function for each (index, value) pair in the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.Each(func(index int, value int) {
+//		fmt.Println(index, value)
+//	})
+func (a *Array[T]) Each(f func(int, T)) {
+	for i, v := range *a {
+		f(i, v)
+	}
+}
+
+// EachIndex calls the given function for each index in the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.EachIndex(func(index int) {
+//		fmt.Println(index)
+//	})
+func (a *Array[T]) EachIndex(f func(int)) {
+	for i := range *a {
+		f(i)
+	}
+}
+
+// EachValue calls the given function for each value in the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.EachValue(func(value int) {
+//		fmt.Println(value)
+//	})
+func (a *Array[T]) EachValue(f func(T)) {
+	for _, v := range *a {
+		f(v)
+	}
+}
+
+// Map returns a new array containing the values returned by the given function for each value in the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.Map(func(value int) int {
+//		return value + 1
+//	})) // [2 3 4]
+func (a *Array[T]) Map(f func(T) T) *Array[T] {
+	var array Array[T]
+	for _, v := range *a {
+		array.Append(f(v))
+	}
+	return &array
+}
+
+// SortBy sorts the array using the sort function (which should be a function that takes two
+// values and returns -1, 0, or 1).
+// -1 means the first value is less than the second value
+// 0 means the first value is equal to the second value
+// 1 means the first value is greater than the second value
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(3)
+//	array.Append(1)
+//	array.Append(2)
+//	array.SortBy(func(a int, b int) int {
+//		if a < b {
+//			return -1
+//		} else if a > b {
+//			return 1
+//		} else {
+//			return 0
+//		}
+//	})
+//	fmt.Println(array) // [1 2 3]
+func (a *Array[T]) SortBy(compare func(T, T) int) {
+	// Using the compare function with QuickSort
+	quickSort(a, 0, len(*a)-1, compare)
+}
+
+// quickSort is a helper function for SortBy
+func quickSort[T constraints.Ordered](a *Array[T], lo int, hi int, compare func(T, T) int) {
+	if lo < hi {
+		p := partition(a, lo, hi, compare)
+		quickSort(a, lo, p-1, compare)
+		quickSort(a, p+1, hi, compare)
+	}
+}
+
+// partition is a helper function for quickSort
+func partition[T constraints.Ordered](a *Array[T], lo int, hi int, compare func(T, T) int) int {
+	pivot := (*a)[hi]
+	i := lo
+	for j := lo; j < hi; j++ {
+		if compare((*a)[j], pivot) < 0 {
+			(*a)[i], (*a)[j] = (*a)[j], (*a)[i]
+			i++
+		}
+	}
+	(*a)[i], (*a)[hi] = (*a)[hi], (*a)[i]
+	return i
+}
+
+func compareAsc[T constraints.Ordered](a T, b T) int {
+	if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func compareDes[T constraints.Ordered](a T, b T) int {
+	if a > b {
+		return -1
+	} else if a < b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+// Sort sorts the array (ascending or descending), default is ascending.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(3)
+//	array.Append(1)
+//	array.Append(2)
+//	array.Sort()
+//	fmt.Println(array) // [1 2 3]
+func (a *Array[T]) Sort(isAscending bool) {
+	// Using QuickSort
+	if isAscending {
+		quickSort(a, 0, len(*a)-1, compareAsc[T])
+	} else {
+		quickSort(a, 0, len(*a)-1, compareDes[T])
+	}
+}
+
+// Reverse reverses the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.Reverse()
+//	fmt.Println(array) // [3 2 1]
+func (a *Array[T]) Reverse() {
+	for i, j := 0, len(*a)-1; i < j; i, j = i+1, j-1 {
+		(*a)[i], (*a)[j] = (*a)[j], (*a)[i]
+	}
+}
+
+// Copy returns a copy of the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	arrayCopy := array.Copy()
+//	fmt.Println(arrayCopy) // [1 2 3]
+func (a *Array[T]) Copy() *Array[T] {
+	var array Array[T]
+	for _, v := range *a {
+		array.Append(v)
+	}
+	return &array
+}
+
+// IndexOf returns the index of the first occurrence of the given value in the array.
+// If the value is not found, -1 is returned.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	fmt.Println(array.IndexOf(2)) // 1
+//	fmt.Println(array.IndexOf(4)) // -1
+func (a *Array[T]) IndexOf(value T) int {
+	for i, v := range *a {
+		if v == value {
+			return i
+		}
+	}
+	return -1
+}
+
+// Insert inserts the given value at the given index.
+// If the index is out of bounds, an error is returned.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.Insert(1, 4)
+//	fmt.Println(array) // [1 4 2 3]
+func (a *Array[T]) Insert(index int, value T) error {
+	if index < 0 || index > len(*a) {
+		return ArrayError{fmt.Errorf("ArrayError: %d index out of range", index)}
+	}
+	*a = append(*a, value)
+	copy((*a)[index+1:], (*a)[index:])
+	(*a)[index] = value
+	return nil
+}
+
+// Remove removes the value at the given index.
+// If the index is out of bounds, an error is returned.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.Remove(1)
+//	fmt.Println(array) // [1 3]
+func (a *Array[T]) Remove(index int) error {
+	if index < 0 || index >= len(*a) {
+		return ArrayError{fmt.Errorf("ArrayError: %d index out of range", index)}
+	}
+	*a = append((*a)[:index], (*a)[index+1:]...)
+	return nil
+}
+
+// RemoveRange removes the values in the range of start and end.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	array.RemoveRange(1, 2)
+//	fmt.Println(array) // [1 3]
+func (a *Array[T]) RemoveRange(start int, end int) {
+	*a = append((*a)[:start], (*a)[end:]...)
+}
+
+// Slice returns a slice of the array.
+//
+// Example:
+//
+//	var array Array[int]
+//	array.Append(1)
+//	array.Append(2)
+//	array.Append(3)
+//	slice := array.Slice(1, 2)
+//	fmt.Println(slice) // [2]
+func (a *Array[T]) Slice(start int, end int) Array[T] {
+	var array Array[T]
+	for i := start; i < end; i++ {
+		array.Append((*a)[i])
+	}
+	return array
+}
+
+// InsertArray inserts the given array at the given index.
+// If the index is out of bounds, an error is returned.
+//
+// Example:
+//
+//	var array1 Array[int]
+//	array1.Append(1)
+//	array1.Append(2)
+//	array1.Append(3)
+//	var array2 Array[int]
+//	array2.Append(4)
+//	array2.Append(5)
+//	array2.Append(6)
+//	array1.InsertArray(1, array2)
+//	fmt.Println(array1) // [1 4 5 6 2 3]
+func (a *Array[T]) InsertArray(index int, array Array[T]) error {
+	if index < 0 || index > len(*a) {
+		return ArrayError{fmt.Errorf("ArrayError: %d index out of range", index)}
+	}
+	*a = append(*a, array...)
+	copy((*a)[index+len(array):], (*a)[index:])
+	copy((*a)[index:], array)
+	return nil
+}
---
+++ array_test.go
@@ -0,0 +1,356 @@
+package gblink
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArray_Len(t *testing.T) {
+	assert := assert.New(t)
+
+	a := Array[int]{1, 2, 3}
+
+	assert.Equal(3, a.Len())
+}
+
+func TestArray_Append(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.Append(1)
+	a.Append(2)
+	a.Append(3)
+
+	assert.Equal(3, a.Len())
+}
+
+func TestArray_AppendAll(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	assert.Equal(3, a.Len())
+}
+
+func TestArray_AppendArray(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendArray(&Array[int]{1, 2, 3})
+
+	assert.Equal(3, a.Len())
+}
+
+func TestArray_AppendArrayAll(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendArrayAll(&Array[int]{1, 2, 3}, &Array[int]{4, 5, 6})
+
+	assert.Equal(6, a.Len())
+}
+
+func TestArray_Clear(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+	a.Clear()
+
+	assert.Equal(0, a.Len())
+}
+
+func TestArray_Contains(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	assert.True(a.Contains(2))
+
+	assert.False(a.Contains(4))
+}
+
+func TestArray_ContainsAll(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	assert.True(a.ContainsAll(1, 2, 3))
+
+	assert.False(a.ContainsAll(1, 2, 4))
+}
+
+func TestArray_Some(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	assert.True(a.Some(func(i int) bool {
+		return i == 2
+	}))
+
+	assert.False(a.Some(func(i int) bool {
+		return i == 4
+	}))
+}
+
+func TestArray_Every(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	assert.True(a.Every(func(i int) bool {
+		return i > 0
+	}))
+
+	assert.False(a.Every(func(i int) bool {
+		return i > 1
+	}))
+
+}
+
+func TestArray_Filter(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.Filter(func(i int) bool {
+		return i > 1
+	})
+
+	assert.Equal(2, b.Len())
+}
+
+func TestArray_Find(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.Find(func(i int) bool {
+		return i > 1
+	})
+
+	assert.Equal(2, b)
+}
+
+func TestArray_FindIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.FindIndex(func(i int) bool {
+		return i > 1
+	})
+
+	assert.Equal(1, b)
+}
+
+func TestArray_FindLast(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.FindLast(func(i int) bool {
+		return i > 1
+	})
+
+	assert.Equal(3, b)
+}
+
+func TestArray_FindLastIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.FindLastIndex(func(i int) bool {
+		return i > 1
+	})
+
+	assert.Equal(2, b)
+}
+
+func TestArray_Each(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	a.Each(func(i int, v int) {
+		assert.Equal(i+1, v)
+	})
+}
+
+func TestArray_EachIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	a.EachIndex(func(i int) {
+		assert.Equal(i, i)
+	})
+}
+
+func TestArray_EachValue(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	a.EachValue(func(v int) {
+		assert.Equal(v, v)
+	})
+}
+
+func TestArray_Map(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	b := a.Map(func(i int) int {
+		return i + 1
+	})
+
+	assert.Equal(3, b.Len())
+}
+
+func TestArray_SortBy(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	a.SortBy(func(i, j int) int {
+		if i > j {
+			return 1
+		}
+
+		if i < j {
+			return -1
+		}
+
+		return 0
+	})
+
+	assert.Equal(1, a[0])
+	assert.Equal(2, a[1])
+	assert.Equal(3, a[2])
+}
+
+func TestArray_Sort(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(1, 2, 3)
+
+	a.Sort(true)
+
+	assert.Equal(1, a[0])
+	assert.Equal(2, a[1])
+	assert.Equal(3, a[2])
+
+	a.Sort(false)
+
+	assert.Equal(3, a[0])
+	assert.Equal(2, a[1])
+	assert.Equal(1, a[2])
+}
+
+func TestArray_Reverse(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	a.AppendAll(3, 1, 2)
+
+	a.Reverse()
+
+	assert.Equal(2, a[0])
+	assert.Equal(1, a[1])
+	assert.Equal(3, a[2])
+}
+
+func TestArray_IndexOf(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+
+	a.AppendAll(1, 2, 3)
+
+	assert.Equal(1, a.IndexOf(2))
+
+	assert.Equal(-1, a.IndexOf(4))
+}
+
+func TestArray_Insert(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+
+	a.AppendAll(1, 2, 3)
+
+	a.Insert(1, 4)
+
+	assert.Equal(4, a[1])
+}
+
+func TestArray_Remove(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+
+	a.AppendAll(1, 2, 3)
+
+	a.Remove(1)
+
+	assert.Equal(3, a[1])
+}
+
+func TestArray_RemoveRange(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+
+	a.AppendAll(1, 2, 3, 4, 5)
+
+	a.RemoveRange(1, 3)
+
+	assert.Equal(4, a[1])
+}
+
+func TestArray_Slice(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+
+	a.AppendAll(1, 2, 3, 4, 5)
+
+	b := a.Slice(1, 3)
+
+	assert.Equal(2, b[0])
+}
+
+func TestArray_InsertArray(t *testing.T) {
+	assert := assert.New(t)
+
+	var a Array[int]
+	var b Array[int]
+
+	a.AppendAll(1, 2, 3)
+	b.AppendAll(4, 5, 6)
+
+	a.InsertArray(1, b)
+
+	assert.Equal(4, a[1])
+}
